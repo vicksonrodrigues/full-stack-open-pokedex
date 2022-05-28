2edcddef80418036162e3e892345534e6c2dc98b
Vickson Rodrigues <55660155+vicksonrodrigues@users.noreply.github.com>
2022-05-28 23:07:10 +0530

    Solved lint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,10 @@
 module.exports = {
   "env": {
     "browser": true,
+    'commonjs': true,
     "es6": true,
-    "jest/globals": true
+    "jest/globals": true,
+    'node': true
   },
   "extends": [
     "eslint:recommended",
